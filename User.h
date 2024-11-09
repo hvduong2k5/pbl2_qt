953bc2c9a2208fbd2b59c91cf9c45d05af5c750d
vcongggggg <vancong030809@gmail.com>
2024-11-09 19:32:44 +0700

    Update User.h

--- User.h
+++ User.h
@@ -2,14 +2,44 @@
 #define USER_H
 
 #include <string>
-#include "Dictionary.h"
-
-class Dictionary;
+#include <iostream>
+using namespace std;
 
 class User {
 private:
-    std::string account;
-    std::string password;
-    std::string gmail;
-    std::string full_name;
+    string account;
+    string password;
+    string gmail;
+    string full_name;
     int role; // 1 = admin, 2 = normal user
+
+public:
+    User();
+    User(const string& account, const string& password, const string& gmail, const string& full_name, int role);
+
+    // Getter và Setter
+    string getAccount() const;
+    void setAccount(const string& acc);
+
+    string getPassword() const;
+    void setPassword(const string& pass);
+
+    string getGmail() const;
+    void setGmail(const string& mail);
+
+    string getFullName() const;
+    void setFullName(const string& name);
+
+    int getRole() const;
+    void setRole(int r);
+
+    bool isRole(int role) const;
+
+    // Phương thức xác thực và hiển thị thông tin
+    bool authenticate(const string& acc, const string& pass) const;
+    void displayInfo() const;
+    void inputInfo();
+};
+
+#endif // USER_H
+
